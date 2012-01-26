934b708285311876622847e2cfd294e64140aa87
Silas Sewell <silas@sewell.org>
2012-01-26 00:30:08 -0500

    Add TestResult model, misc other model work, and fix package.json

--- test/assets/helper.js
+++ test/assets/helper.js
@@ -1,5 +1,6 @@
 var async = require('async')
   , global = require('../../lib/ntfserver/global')
+  , models = require('../../lib/ntfserver/models')
   , mysql = require('mysql')
 
 exports.setUpSql = function(cb) {
@@ -28,4 +29,47 @@ exports.tearDownSql = function(cb) {
   try { global.sql.destroy() } catch(err) {}
   global.sql = null
   cb()
+}
+
+var fixtureTypes = ['agent', 'suite', 'test']
+
+var fixtureFunctions = {
+}
+
+exports.setUpFixtures = function(setup, cb) {
+  var work = [
+    function(cb) { cb(null, {}) }
+  ]
+
+  if (setup.agent) {
+    work.push(function(context, cb) {
+      context.name = context.agent_name = 'agent'
+      models.Agent.getOrInsert(context, function(err, id) {
+        context.agent_id = id
+        cb(err, context)
+      })
+    })
+  }
+
+  if (setup.suite || setup.test) {
+    work.push(function(context, cb) {
+      context.name = context.suite_name = 'suite'
+      models.Suite.getOrInsert(context, function(err, id) {
+        context.suite_id = id
+        cb(err, context)
+      })
+    })
+  }
+
+  if (setup.test) {
+    work.push(function(context, cb) {
+      context.name = context.test_name = 'test'
+      models.Test.getOrInsert(context, function(err, id) {
+        context.test_id = id
+        cb(err, context)
+      })
+    })
+  }
+
+  async.waterfall(work, cb)
 }
--- test/models.js
+++ test/models.js
@@ -9,27 +9,27 @@ exports.agentGetOrInsert = function(test) {
   async.series([
     // insert
     function(cb) {
-      models.Agent.getOrInsert({ name: 'agent' }, function(err, obj) {
+      models.Agent.getOrInsert({ name: 'agent' }, function(err, id) {
         if (err) throw err
-        test.equal(obj.id, 1)
+        test.equal(id, 1)
         cb()
       })
     },
     // get from cache
     function(cb) {
-      models.Agent.cache.agent.id = 2
-      models.Agent.getOrInsert({ name: 'agent' }, function(err, obj) {
+      models.Agent.cache.agent = 2
+      models.Agent.getOrInsert({ name: 'agent' }, function(err, id) {
         if (err) throw err
-        test.equal(obj.id, 2)
+        test.equal(id, 2)
         cb()
       })
     },
     // get from database
     function(cb) {
       models.Agent.cache = {}
-      models.Agent.getOrInsert({ name: 'agent' }, function(err, obj) {
+      models.Agent.getOrInsert({ name: 'agent' }, function(err, id) {
         if (err) throw err
-        test.equal(obj.id, 1)
+        test.equal(id, 1)
         cb()
       })
     },
@@ -42,27 +42,27 @@ exports.suiteGetOrInsert = function(test) {
   async.series([
     // insert
     function(cb) {
-      models.Suite.getOrInsert({ name: 'suite' }, function(err, obj) {
+      models.Suite.getOrInsert({ name: 'suite' }, function(err, id) {
         if (err) throw err
-        test.equal(obj.id, 1)
+        test.equal(id, 1)
         cb()
       })
     },
     // get from cache
     function(cb) {
-      models.Suite.cache.suite.id = 2
-      models.Suite.getOrInsert({ name: 'suite' }, function(err, obj) {
+      models.Suite.cache.suite = 2
+      models.Suite.getOrInsert({ name: 'suite' }, function(err, id) {
         if (err) throw err
-        test.equal(obj.id, 2)
+        test.equal(id, 2)
         cb()
       })
     },
     // get from database
     function(cb) {
       models.Suite.cache = {}
-      models.Suite.getOrInsert({ name: 'suite' }, function(err, obj) {
+      models.Suite.getOrInsert({ name: 'suite' }, function(err, id) {
         if (err) throw err
-        test.equal(obj.id, 1)
+        test.equal(id, 1)
         cb()
       })
     },
@@ -72,34 +72,80 @@ exports.suiteGetOrInsert = function(test) {
 }
 
 exports.testGetOrInsert = function(test) {
-  async.series([
+  async.waterfall([
+    // setup
+    function(cb) { helper.setUpFixtures({ suite: 1 }, cb) },
     // insert
-    function(cb) {
-      models.Test.getOrInsert({ suite: 'suite', name: 'test' }, function(err, obj) {
-        if (err) throw err
-        test.equal(obj.id, 1)
-        cb()
+    function(context, cb) {
+      models.Test.getOrInsert({ suite_id: context.suite_id, name: 'test' }, function(err, id) {
+        if (err) return cb(err)
+        test.equal(id, 1)
+        cb(null, context)
       })
     },
     // get from cache
-    function(cb) {
-      models.Test.cache['suite|test'].id = 2
-      models.Test.getOrInsert({ suite: 'suite', name: 'test' }, function(err, obj) {
-        if (err) throw err
-        test.equal(obj.id, 2)
-        cb()
+    function(context, cb) {
+      models.Test.cache[context.suite_id + '|test'] = 2
+      models.Test.getOrInsert({ suite_id: context.suite_id, name: 'test' }, function(err, id) {
+        if (err) return cb(err)
+        test.equal(id, 2)
+        cb(null, context)
       })
     },
     // get from database
-    function(cb) {
+    function(context, cb) {
       models.Test.cache = {}
-      models.Test.getOrInsert({ suite: 'suite', name: 'test' }, function(err, obj) {
-        if (err) throw err
-        test.equal(obj.id, 1)
+      models.Test.getOrInsert({ suite_id: context.suite_id, name: 'test' }, function(err, id) {
+        if (err) return cb(err)
+        test.equal(id, 1)
         cb()
       })
     },
-  ], function(err, result) {
+  ], function(err) {
+    if (err) throw err
+    test.done()
+  })
+}
+
+exports.testResultGetOrInsert = function(test) {
+  async.waterfall([
+    // setup
+    function(cb) {
+      helper.setUpFixtures({ agent: 1, test: 1 }, cb)
+    },
+    // insert
+    function(context, cb) {
+      context.name = 'test'
+      context.duration = 123
+      context.passes = 8
+      context.failures = 2
+      context.time = 1327555445
+      models.TestResult.getOrInsert(context, function(err, id) {
+        if (err) return cb(err)
+        test.equal(id, 1)
+        cb(null, context)
+      })
+    },
+    // get from cache
+    function(context, cb) {
+      models.Test.cache[context.suite_id + '|' + context.name] = 2
+      models.Test.getOrInsert(context, function(err, id) {
+        if (err) return cb(err)
+        test.equal(id, 2)
+        cb(null, context)
+      })
+    },
+    // get from database
+    function(context, cb) {
+      models.Test.cache = {}
+      models.Test.getOrInsert(context, function(err, id) {
+        if (err) return cb(err)
+        test.equal(id, 1)
+        cb()
+      })
+    },
+  ], function(err) {
+    if (err) throw err
     test.done()
   })
 }
