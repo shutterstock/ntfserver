a4e3ff8498758356230b7e5a9ae9f9a5a2d0a20d
Silas Sewell <silas@sewell.org>
2012-02-11 11:55:00 -0500

    Rename passes/failures to pass/fail

--- test/store.js
+++ test/store.js
@@ -37,8 +37,8 @@ exports.handleSuite = function(test) {
       if (err) return cb(err)
       test.equal(results.length, 1)
       test.equal(results[0].duration, 36)
-      test.equal(results[0].passes, 7)
-      test.equal(results[0].failures, 1)
+      test.equal(results[0].pass, 7)
+      test.equal(results[0].fail, 1)
       test.equal(results[0].time, 1327609606)
       cb()
     })
@@ -58,7 +58,7 @@ exports.handleSuite = function(test) {
   work.push(function(cb) {
     global.sql.query(
       'SELECT t.name AS name, tr.duration AS duration,' +
-      '  tr.passes AS passes, tr.failures AS failures' +
+      '  tr.pass AS pass, tr.fail AS fail' +
       '  FROM test_result tr' +
       '  LEFT JOIN test t ON ' +
       '  t.test_id = tr.test_id ORDER BY t.name', [],
@@ -66,14 +66,14 @@ exports.handleSuite = function(test) {
       if (err) return cb(err)
       test.equal(results.length, 3)
       test.equal(results[0].duration, 10)
-      test.equal(results[0].passes, 3)
-      test.equal(results[0].failures, 0)
+      test.equal(results[0].pass, 3)
+      test.equal(results[0].fail, 0)
       test.equal(results[1].duration, 15)
-      test.equal(results[1].passes, 2)
-      test.equal(results[1].failures, 0)
+      test.equal(results[1].pass, 2)
+      test.equal(results[1].fail, 0)
       test.equal(results[2].duration, 11)
-      test.equal(results[2].passes, 2)
-      test.equal(results[2].failures, 1)
+      test.equal(results[2].pass, 2)
+      test.equal(results[2].fail, 1)
       cb()
     })
   })
